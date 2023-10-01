7a66bd4f2cb67ee5b54e75d9a63e7315d362aa18
Burak <99614754+burakkertn@users.noreply.github.com>
2023-10-01 13:17:02 +0300

    ..

--- TrainStationProject/Models/RegisterViewModel.cs
+++ TrainStationProject/Models/RegisterViewModel.cs
@@ -10,9 +10,6 @@ namespace TrainStationProject.Models
         [Required(ErrorMessage = "Lütfen şifreyi giriniz")]
         public string Password { get; set; }
 
-        //[Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz")]
-        //[Compare("Password", ErrorMessage = "Şifreler uyumlu değil, kontrol ediniz")]
-        //public string passwordConfirm { get; set; }
     }
 }
 
